5693720c210d5f5fb5ad24f7d29efa0ca537ab10
g-kohl <kohlguilherme2004@gmail.com>
2025-01-15 17:16:45 -0300

    revising round 952

--- contests/codeforces/round952/e.cpp
+++ contests/codeforces/round952/e.cpp
@@ -1,28 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void solve(){
+    int x, y, z;
+    long long k, quant=0;
+
+    cin >> x >> y >> z >> k;
+
+    for(long long a=1; a<=x; a++)
+        for(long long b=1; b<=y; b++){
+            long long c = k / (a*b);
+
+            if(a*b*c == k && c <= z)
+                quant = max(quant, (x-a+1) * (y-b+1) * (z-c+1));
+        }
+
+    cout << quant << "\n";
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    long long t, x, y, z, k, a, b, c, quant;
+    int t=1;
+    cin >> t;
 
-    for(cin>>t; t>0; t--){
-        cin >> x >> y >> z >> k;
-
-        quant = 0;
-
-        for(a=1; a<=x; a++){
-            for(b=1; b<=y; b++){
-                if(k % (a*b) == 0 && k/(a*b) <= z){
-                    c = k/(a*b);
-
-                    quant = max(quant, (x-a+1)*(y-b+1)*(z-c+1));
-                }
-            }
-        }
-
-        cout << quant << "\n";
+    while(t--){
+        solve();
     }
 
     return 0;
--- contests/codeforces/round952/f.cpp
+++ contests/codeforces/round952/f.cpp
@@ -1,64 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool checker(long long turns, vector<long long> &a, vector<long long> &c, long long h, long long n){
+bool canKill(vector<int> &a, vector<int> &c, long long turn, int h, int n){
     long long damage=0;
 
     for(int i=0; i<n; i++)
-        damage += (turns/c[i]) * a[i];
+        damage += (turn/c[i]) * a[i];
 
-    if(damage >= h)
-        return true;
+    return (damage >= h) ? true : false;
+}
 
-    return false;   
+void solve(){
+    int n;
+    long long h, turns, l=1, r=40000000000, mid;
+
+    cin >> h >> n;
+
+    vector<int> a(n), c(n);
+
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+
+        h -= a[i];
+    }
+
+    for(int i=0; i<n; i++)
+        cin >> c[i];
+
+    if(h <= 0)
+        cout << "1\n";
+    else{
+        while(l <= r){
+            mid = l + (r - l) / 2;
+
+            if(canKill(a, c, mid, h, n) && !canKill(a, c, mid-1, h, n)){
+                turns = mid;
+                break;
+            }
+            else if(canKill(a, c, mid, h, n) && canKill(a, c, mid-1, h, n))
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+
+        cout << turns+1 << "\n";
+    }
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    long long t, h, n, turns, l, r, mid;
-    bool check;
+    int t=1;
+    cin >> t;
 
-    for(cin>>t; t>0; t--){
-        turns = 0;
-        l = 1;
-        r = 40000000000;
-
-        cin >> h >> n;
-
-        vector<long long> a(n), c(n);
-
-        for(int i=0; i<n; i++)
-            cin >> a[i];
-
-        for(int i=0; i<n; i++)
-            cin >> c[i];
-
-        for(int i=0; i<n; i++)
-            h -= a[i];
-
-        if(h <= 0)
-            cout << "1\n";
-        else{    
-            while(l <= r){
-                mid = (l+r)/2;
-
-                check = checker(mid, a, c, h, n);
-
-                if(check){
-                    r = mid - 1;
-                    turns = mid;
-                }
-                else
-                    l = mid + 1;
-            }
-
-            cout << turns+1 << "\n";
-        }
-
-        a.clear();
-        c.clear();
+    while(t--){
+        solve();
     }
 
     return 0;
